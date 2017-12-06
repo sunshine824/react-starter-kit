1b52c5d27814c7e39da29c0f6d00e29e428b7f4b
Administrator <790168710@qq.com>
2017-12-06 14:41:08 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import RouteMap from './router/routeMap'
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            initDone: false
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            initDone: true
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.initDone
+                        ? <RouteMap/>
+                        : '正在加载...'
+                }
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/containers/Home/index.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+import {testData} from '../../fetch/test'
+
+import './style.less'
+
+export default class Home extends Component {
+    constructor() {
+        super()
+    }
+
+    componentDidMount(){
+        //fetch请求测试
+        const result=testData('494a4b33584f3652355a686e7957344a4e48535578413d3d0a')
+        result.then((res)=>{
+            return res.json()
+        }).then(json=>{
+            console.log(json)
+        }).catch((err)=>{
+            console.log(err.message)
+        })
+    }
+
+    render(){
+        return(
+            <div className="test">
+                <div className="title">首页</div>
+            </div>
+        )
+    }
+}
